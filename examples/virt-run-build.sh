1feab4fe2bb6a4b2357f44ec0f39bd9811e53efb
Michael Meffie <mmeffie@sinenomine.net>
2018-05-09 23:01:09 -0400

    update examples

--- examples/virt-run-build.sh
+++ examples/virt-run-build.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
 virt-run \
-  --base ubun18 \
+  --base ubuntu1804 \
   --base-prefix "zz-" \
   --clone-prefix "tmp-" \
   --user mmeffie \
+  --wait 30 \
   "uname -a" \
   "gcc --version" \
   "kernel-ppa get" \
-  "kernel-ppa install" \
+  "sudo kernel-ppa install" \
   "@reboot" \
   "gcc --version" \
-  "@say git clone https://gerrit.openafs.org/openafs.git" \
+  "git clone https://gerrit.openafs.org/openafs.git" \
   "@cd openafs" \
-  "@say ./regen.sh" \
-  "@say ./configure --enable-debug --enable-debug-kernel --disable-optimize
-        --disable-optimize-kernel --without-dot --enable-transarc-paths
-        --with-linux-kernel-packaging --enable-checking" \
-  "@say make -j 1" \
+  "./regen.sh" \
+  "./configure --enable-debug --enable-debug-kernel --disable-optimize --disable-optimize-kernel" \
+  "make -j 1" \
   "@say all done"
-
--- examples/virt-run-test.sh
+++ examples/virt-run-test.sh
@@ -3,7 +3,7 @@
 # Example virt-run command to spin a guest vm, run some commands,
 # then delete the guest vm.
 virt-run \
-  --base ubun18 \
+  --base ubuntu1804 \
   --base-prefix "zz-" \
   --clone-prefix "tmp-" \
   "uname -a" \
