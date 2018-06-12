ce720427c92058fb6bf6b1d667926ebe47de9aa7
Michael Meffie <mmeffie@sinenomine.net>
2018-06-12 18:57:13 -0400

    virt-run: add @onfail magic command
    Add @onfail command to run commands on failure before deleting the
    ephemeral guest.  This was added so we can see the configure log on
    build failures.
    
    Also update the virt-run examples.

--- examples/virt-run-build.sh
+++ examples/virt-run-build.sh
@@ -1,20 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
-virt-run \
-  --base ubuntu1804 \
-  --base-prefix "zz-" \
-  --clone-prefix "tmp-" \
-  --user mmeffie \
-  --wait 30 \
-  "uname -a" \
-  "gcc --version" \
-  "kernel-ppa get" \
-  "sudo kernel-ppa install" \
-  "@reboot" \
-  "gcc --version" \
-  "git clone https://gerrit.openafs.org/openafs.git" \
-  "@cd openafs" \
-  "./regen.sh" \
-  "./configure --enable-debug --enable-debug-kernel --disable-optimize --disable-optimize-kernel" \
-  "make -j 1" \
-  "@say all done"
+BASE_NAME="ubuntu1804"
+BASE_PREFIX="zz-"
+REMOTE_USER="$USER"
+
+opt_ref="master"
+opt_ppa=""
+
+usage() {
+    cat >&2 <<__EOF__
+usage: virt-run-build.sh [--ref <ref>] [--linux <linux>]
+
+where:
+  <ref> is the git branch or gerrit ref (default: ${opt_ref})
+  <linux> is the linux ppa version (default: current rc)
+
+examples:
+
+   virt-run-build.sh  # master, most recent rc
+   virt-run-build.sh --ref openafs-stable-1_8_x --linux daily/current
+   virt-run-build.sh --ref refs/heads/34/1234/1 --linux v4.17-rc4
+
+__EOF__
+}
+
+while [ "x$1" != "x" ]; do
+    case "$1" in
+    --ref)  opt_ref="$2"; shift 2;;
+    --linux) opt_ppa="$2"; shift 2;;
+    -h|--help) usage; exit 0;;
+    *)  usage; exit 1;;
+    esac
+done
+
+/usr/local/bin/virt-run \
+    --base "$BASE_NAME" \
+    --base-prefix "$BASE_PREFIX" \
+    --clone-prefix "tmp-" \
+    --user "$REMOTE_USER" \
+    --wait "30" \
+    "kernel-ppa get ${opt_ppa}" \
+    "sudo kernel-ppa install" \
+    "@reboot" \
+    "uname -a" \
+    "gcc --version" \
+    "mkdir openafs" \
+    "@cd openafs" \
+    "git init" \
+    "git fetch https://gerrit.openafs.org/openafs.git ${opt_ref}" \
+    "git reset --hard FETCH_HEAD" \
+    "git --no-pager log -n1 --stat" \
+    "./regen.sh" \
+    "./configure" \
+    "make" \
+    "@onfail cat config.log"
--- examples/virt-run-test.sh
+++ examples/virt-run-test.sh
@@ -2,10 +2,11 @@
 
 # Example virt-run command to spin a guest vm, run some commands,
 # then delete the guest vm.
-virt-run \
-  --base ubuntu1804 \
+../virt-run \
+  --base "ubuntu1804" \
   --base-prefix "zz-" \
   --clone-prefix "tmp-" \
+  "@onfail echo something bad happened" \
   "uname -a" \
   "gcc --version" \
   "@sleep 2" \
